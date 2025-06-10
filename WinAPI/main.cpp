71e5a794d3226d607d77beb9a72fd70114b4aef8
Veronika050987 <veronicaviv@mail.ru>
2025-06-11 00:15:49 +0300

    login changing with errors DONE

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,60 +1,96 @@
 ﻿#include<Windows.h>
+#include<Commctrl.h>
 #include"resource.h"
+
+#define LOGIN_PROMPT L"Enter user's name" //при нажатии на поле пароля, незаполнении поля логина и нажатии на кнопку ОК выдаётся сообщение 
+                                           //об ошибке "Введите логин"
+                                           //при незаполнении поля логина и нажатии кнопки ОК выдаётся сообщение об ошибке "Введите логин"
+                                           //при не изменении поля логина и нажатии кнопки ОК выдаётся сообщение об ошибке "Введите логин"
+                                           //выйти можно только с помощью клавиши Cancel
+                                           //текст в поле логина стирается руками
+#define LOGIN_ERROR_MSG L"Заполните поле \"Login\"!"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-//#define MESSAGE_BOX
-
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-
-
-#ifdef MESSAGE_BOX
-	MessageBox
-	(
-		NULL,
-		"Привет WinAPI!\nЭто самое простое окно - окно сообщения\t\t\t\t(MessageBox)",
-		"Привет!",
-		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION //| MB_HELP
-		| MB_DEFBUTTON4
-		| MB_TOPMOST
-	);
-	//MB_ - MessageBox
-	//Венгерска нотация    
-#endif // MESSAGE_BOX
-
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
-	return 0;
+    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+    return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-	case WM_INITDIALOG://Выполняется один раз - при запуске окна !!!!!!
-		break;
-	case WM_COMMAND://Обрабатывает нажатие кнопок, перемещение мыши и т.д.
-		switch (LOWORD(wParam))
-		{
-		case IDC_BUTTON_COPY:
-		{
-			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);//отсюда считываем
-			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);//сюда копируем
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE] = {};
-			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-		}	
-		break;
-		case IDOK:
-			MessageBox(hwnd, "Была нажата кнопка 'OK'", "Info", MB_OK | MB_ICONINFORMATION);
-			break;
-		case IDCANCEL:EndDialog(hwnd, 0); break;
-		}
-		break;
-	case WM_CLOSE://Отрабатывает при нажатии на кнопку "Закрыть" X
-		EndDialog(hwnd, 0);
-		break;
-	}
-	return FALSE;
+    HWND hEditLogin = NULL;
+    HWND hEditPassword = NULL;
+
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+        hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+        SendMessageW(hEditLogin, EM_SETLIMITTEXT, 31, 0);
+        SetWindowTextW(hEditLogin, LOGIN_PROMPT); 
+    }
+    break;
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+        {
+            WCHAR loginBuffer[256];
+            GetWindowTextW(hEditLogin, loginBuffer, 256);
+
+            if (wcscmp(loginBuffer, LOGIN_PROMPT) == 0 || loginBuffer[0] == L'\0')
+            {
+                MessageBoxW(hwnd, LOGIN_ERROR_MSG, L"Validation Error", MB_OK | MB_ICONERROR);
+                SetFocus(hEditLogin);
+                return TRUE;
+            }
+
+            MessageBoxW(hwnd, L"OK button was pressed", L"Info", MB_OK | MB_ICONINFORMATION);
+            EndDialog(hwnd, 0);
+            break;
+        }
+        case IDCANCEL:
+            EndDialog(hwnd, 0);
+            break;
+        }
+        break;
+    }
+    case WM_NOTIFY:
+    {
+        if (((LPNMHDR)lParam)->code == EN_SETFOCUS && ((LPNMHDR)lParam)->hwndFrom == GetDlgItem(hwnd, IDC_EDIT_LOGIN))
+        {
+            HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+            WCHAR buffer[256];
+            GetWindowTextW(hEditLogin, buffer, 256);
+
+            if (wcscmp(buffer, LOGIN_PROMPT) == 0)
+            {
+                SetWindowTextW(hEditLogin, L"");
+            }
+        }
+        else if (((LPNMHDR)lParam)->code == EN_KILLFOCUS && ((LPNMHDR)lParam)->hwndFrom == GetDlgItem(hwnd, IDC_EDIT_LOGIN))
+        {
+            HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+            const int SIZE = 256;
+            WCHAR sz_buffer[SIZE] = {};
+            GetWindowTextW(hEditLogin, sz_buffer, SIZE);
+
+            if (sz_buffer[0] == L'\0')
+            {
+                SetWindowTextW(hEditLogin, LOGIN_PROMPT);
+            }
+        }
+        break;
+    }
+    case WM_CLOSE:
+        EndDialog(hwnd, 0);
+        break;
+    default:
+        return FALSE;
+    }
+    return TRUE;
 }
