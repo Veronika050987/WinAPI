4f0077f96c0fcc5a11e0dc976e5df18ad5a08a40
Veronika050987 <veronicaviv@mail.ru>
2025-07-01 22:22:41 +0300

    fonts in progress

---
+++ FontsDLL/resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+// Next default values for new objects
+// 
+#ifndef FONT_RESOURCE_H
+#define FONT_RESOURCE_H
+
+#define ASTRONAU 203
+#define SUNSET   204
+
+#endif
